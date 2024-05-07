3df06599e658d25efed1d8bff2437ad7e48e0911
Olena Horovenko <elena.gorovenko@gmail.com>
2024-05-07 14:54:15 +0200

    done without bonuses

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+const displayElm = document.querySelector('.display');
+const numButtons = document.querySelectorAll('.btn-num');
 
+const handleDigitClick = (e) => {
+  const buttonValue = e.target.textContent;
+  if (displayElm.textContent.length === 9) {
+    return
+  }
+  displayElm.textContent += buttonValue;
+}
+
+numButtons.forEach((numButton) => {
+  numButton.addEventListener('click', handleDigitClick) 
+});
